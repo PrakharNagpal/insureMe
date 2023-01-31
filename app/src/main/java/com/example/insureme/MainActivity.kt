428ee5663f6ddb5c5cae30256e672c20753fc3ac
Prakhar Nagpal <53858906+PrakharNagpal@users.noreply.github.com>
2023-01-31 14:44:58 +0530

    Added USER LoginPage

--- app/src/main/java/com/example/insureme/MainActivity.kt
+++ app/src/main/java/com/example/insureme/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.insureme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pre_login_signup)
+    }
+    fun onUserLoginClick(view: View) {
+        val moveToLoginScreen=findViewById<Button>(R.id.userLoginButton)
+        moveToLoginScreen.setOnClickListener{
+            startActivity(Intent(this,UserLoginActivity::class.java))
+        }
+        // Do something on user login button click
     }
 }
